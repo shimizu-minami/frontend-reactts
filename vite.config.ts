9306b0e5272a65d02cc2ff512a6a52545c339df4
shimizu-minami <shimizu.minami.public.hall@gmail.com>
2026-04-15 16:00:02 +0900

    TailwindCSS の機能を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 追加
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+	react(),
+	tailwindcss(), // 追加
+],
   server: {
     host: '0.0.0.0', // 0.0.0.0を指定することでコンテナ外からアクセス可能にする
     port: 5173,
